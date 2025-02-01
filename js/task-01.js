af121ad0a9571c00e7adb24b8e43f6e75396923f
uksusit <140089654+uksusit@users.noreply.github.com>
2025-02-01 15:24:35 +0200

    01.1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = document.querySelector(".categories");
+console.log(list);
+
+const ul_categories = categories.querySelectorAll(".item");
+console.log(ul_categories);
