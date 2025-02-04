9311408b388077ecafbbaf46c436b4ddb2e52611
uksusit <140089654+uksusit@users.noreply.github.com>
2025-02-04 22:15:53 +0200

    02var.1ok

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,26 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
+  // "Pumpkin",
 ];
+
+//  var.1
+  console.log("items are ", ingredients.length);
+
+ingredients.forEach(ingredient => {
+  console.log("ingredient: ", ingredient);
+const listEls = document.querySelector('#ingredients');
+// console.log(listEls);
+
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add("item");
+  listEls.append(li);
+  console.log("li.textContent: ", li.textContent);
+console.log("item number ", listEls.querySelectorAll('li').length);
+});
+
+// var.2
+
+// const liEl = `<li class="item">ingredient</li>`
+// listEls.insertAdjacentHTML('beforeend', li);
