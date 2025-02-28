acdd88038a5d1519449ae1c7265a1e2fc79e4a54
uksusIT <140089654+uksusit@users.noreply.github.com>
2025-02-28 21:42:20 +0200

    04ttp get data

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,19 @@
-const counterValue = 0;
+let counterValue = 0;
 
-// const countDec = document.querySelector('button');
-const count = document.querySelector('#counter'.childNodes);
-
+const counts = document.querySelectorAll('button');
+// const countDecr = document.querySelector('#counter'.childNodes);
+counts.forEach(count => {    
 count.addEventListener('click', onClick);
+});
 
 function onClick(evt){
-console.log(evt.currentTarget
-);
-};
+console.dir(evt.currentTarget);
 
+let val = evt.currentTarget;
+
+
+console.log(val.);
+counterValue += 1;
+console.log(counterValue);
+};
 
