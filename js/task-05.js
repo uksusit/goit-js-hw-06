910f3c589d7885694aedf6a2227ad3f9a144d86f
uksusit <140089654+uksusit@users.noreply.github.com>
2025-03-25 20:41:50 +0200

    05tttmp

--- js/task-05.js
+++ js/task-05.js
@@ -6,16 +6,21 @@
 //  у спані повинен відображатися рядок "Anonymous".
 
 let  inpt = document.getElementById('name-input')
-console.log(inpt);
-console.dir(Event.currentTarget);
+console.dir(inpt.currentTarget);
+
 
 const outpt = document.querySelector('#name-output');
 console.log(outpt);
 // outpt.textContent = ({inpt});
 
-inpt.addEventListener("input", (Event) => {
-    outpt.textContent = Event.currentTarget.value;
-});
 
+inpt.addEventListener("input", (Event) => {
+    console.dir(Event.currentTarget);
+    // console.log(Event());
+    if ( inpt.value != "anonymous")  // who must be?
+    outpt.textContent = Event.currentTarget.value;
+   
+ else  outpt.textContent = "Anonymous";
+});
 
 
